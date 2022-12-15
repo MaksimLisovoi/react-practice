28155ecb5b6c3ba6ce507af2bc619eb0aa96bd8f
MaksimLisovoi <lisovoi123@gmail.com>
2022-12-15 22:01:36 +0200

    deleted useless component froom last branch

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,54 +1,10 @@
 import React, { Component } from 'react';
-import { FeedbackOptions } from './FeedbackOptions';
-import { Section } from './Section';
-import { Statistics } from './Statistics';
-import { Notification } from './Notification/Notification';
 import { Box } from './Box';
 
 export class App extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
-  };
-
-  leaveFeedback = e => {
-    this.setState(prevState => ({
-      [e.target.name]: prevState[e.target.name] + 1,
-    }));
-  };
-
-  totalFeedBack = () => this.state.good + this.state.neutral + this.state.bad;
-
-  positivePercentage = () => {
-    return Math.round((this.state.good * 100) / this.totalFeedBack());
-  };
+  state = {};
 
   render() {
-    const { good, bad, neutral } = this.state;
-    const total = this.totalFeedBack();
-    const positive = this.positivePercentage();
-
-    return (
-      <Box mx="auto" maxWidth={450} p={'5'}>
-        <Section title="Please leave your feedback">
-          <FeedbackOptions onLeaveFeedback={this.leaveFeedback} options={this.state} />
-        </Section>
-
-        <Section title="Statistics">
-          {total === 0 ? (
-            <Notification message="There is no feedback"></Notification>
-          ) : (
-            <Statistics
-              good={good}
-              bad={bad}
-              neutral={neutral}
-              total={total}
-              positivePercentage={positive}
-            />
-          )}
-        </Section>
-      </Box>
-    );
+    return <Box mx="auto" maxWidth={450} p={'5'}></Box>;
   }
 }
--- src/components/Statistics/Statistics.styled.js
+++
@@ -1,9 +0,0 @@
-import styled from '@emotion/styled';
-
-export const List = styled.ul``;
-
-export const Item = styled.li``;
-
-export const Text = styled.p``;
-
-export const Number = styled.p``;
