75c08247bf86728fcff50b7d7df62a806dca47c1
MaksimLisovoi <lisovoi123@gmail.com>
2022-12-15 12:11:41 +0200

    added onClick

--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -2,7 +2,9 @@ import { ControlsBlock, Btn } from './FeedbackOptions.styled';
 
 export const FeedbackOptions = () => (
   <ControlsBlock>
-    <Btn type="button">Good</Btn>
+    <Btn onClick={() => {}} type="button">
+      Good
+    </Btn>
     <Btn type="button">Neutral</Btn>
     <Btn type="button">Bad</Btn>
   </ControlsBlock>
