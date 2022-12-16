954640971e8ce371df45187c990f17c05c821758
MaksimLisovoi <lisovoi123@gmail.com>
2022-12-16 13:27:50 +0200

    added form

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -12,13 +12,16 @@ export class App extends Component {
     return (
       <Box mx="auto" maxWidth={450} p={'5'}>
         <Heading>Name</Heading>
-        <input
-          type="text"
-          name="name"
-          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-          required
-        />
+        <form>
+          <input
+            type="text"
+            name="name"
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            required
+          />
+          <button type="submit">Add contact</button>
+        </form>
       </Box>
     );
   }
