bf664a57eda6cf7df73411ad44d91664620a6c37
MaksimLisovoi <lisovoi123@gmail.com>
2023-01-12 16:15:55 +0200

    proptypes +

--- src/components/ColorPicker/ColorPicker.jsx
+++ src/components/ColorPicker/ColorPicker.jsx
@@ -2,6 +2,7 @@
 import { useState } from 'react';
 import classNames from 'classnames';
 import './ColorPicker.css';
+import PropTypes from 'prop-types';
 
 export function ColorPicker({ options }) {
   const [activeOptionIdx, setActiveOptionIdx] = useState(4);
@@ -40,62 +41,6 @@ export function ColorPicker({ options }) {
   );
 }
 
-// export class ColorPicker extends Component {
-//   state = {
-//     activeOptionIdx: 4,
-//   };
-
-//   makeOptionClassName = index => {
-//     const { activeOptionIdx } = this.state;
-
-//     return classNames('ColorPicker__option', {
-//       'ColorPicker__option--active': index === activeOptionIdx,
-//     });
-
-//     // const optionClasses = ['ColorPicker__option'];
-
-//     // if (activeOptionIdx === index) {
-//     //   optionClasses.push('ColorPicker__option--active');
-//     // }
-
-//     // return optionClasses.join(' ');
-//   };
-
-//   setActiveIndex = index => {
-//     this.setState({
-//       activeOptionIdx: index,
-//     });
-//   };
-
-//   render() {
-//     const { options } = this.props;
-//     const { activeOptionIdx } = this.state;
-
-//     const { label } = options[activeOptionIdx];
-
-//     return (
-//       <div className="ColorPicker">
-//         <h2 className="ColorPicker__title">Color Picker</h2>
-
-//         <p>Выбран цвет:{label} </p>
-
-//         <div>
-//           {options.map(({ label, color }, index) => {
-//             return (
-//               <button
-//                 key={label}
-//                 className={this.makeOptionClassName(index)}
-//                 style={{
-//                   backgroundColor: color,
-//                 }}
-//                 onClick={() => {
-//                   this.setActiveIndex(index);
-//                 }}
-//               ></button>
-//             );
-//           })}
-//         </div>
-//       </div>
-//     );
-//   }
-// }
+ColorPicker.propTypes = {
+  options: PropTypes.arrayOf(PropTypes.objectOf(PropTypes.string)),
+};
