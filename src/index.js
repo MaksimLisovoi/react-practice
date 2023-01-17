950374abec7d25709b79e4a24889a140fe87f321
MaksimLisovoi <lisovoi123@gmail.com>
2023-01-17 15:40:09 +0200

    ++

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import './index.css';
 import { App } from 'components/App/App';
 import { ThemeProvider } from '@emotion/react';
 import { theme } from './constants/theme';
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <ThemeProvider theme={theme}>
+//       <App />
+//     </ThemeProvider>
+//   </React.StrictMode>,
+// );
 
 ReactDOM.render(
   <React.StrictMode>
